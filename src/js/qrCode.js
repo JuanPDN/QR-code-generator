7f56beee900934745b8ebd86a071f0eb8cb5fc94
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-04-15 09:34:34 -0500

    functions copy and download

--- src/js/qrCode.js
+++ src/js/qrCode.js
@@ -1,10 +1,33 @@
 const btnDownload = document.querySelector(".download");
 const btnShare = document.querySelector(".share");
 const QR = document.querySelector("#qrcode");
-
 const url = sessionStorage.getItem("url");
 
 var qrcode = new QRCode(QR);
 
-qrcode.makeCode(url);
-// qrcode.clear();
+if (!url.length == 0) {
+  qrcode.makeCode(url);
+} else {
+  qrcode.clear();
+}
+
+const downloadQR = () => {
+  const imageURL = document.querySelector("#qrcode img").src;
+  const filename = `QR${Date.now()}.jpg`;
+
+  const link = document.createElement("a");
+  link.href = imageURL;
+  link.download = filename;
+
+  link.click();
+
+  link.remove();
+};
+
+const copyQR = () => {
+  const imageURL = document.querySelector("#qrcode img").src;
+  navigator.clipboard.writeText(imageURL);
+};
+
+btnDownload.addEventListener("click", downloadQR);
+btnShare.addEventListener("click", copyQR);
